f7a6e053595132f2da9d15ac033d797334b1ee15
koryr5995 <122232214+koryr5995@users.noreply.github.com>
2023-01-31 10:49:18 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,57 @@
+//#1
+/*function maxOfTwoNumbers(x, y) {
+  if (x >= y) {
+    return x;
+  } else {
+    return y;
+  }}
+console.log(maxOfTwoNumbers(5,5));*/
+
+//#2
+/*const maxOfThree = (x, y, z) => {
+  if (x > (y && z)) {
+    return x;
+  } else if (y > (x && z)) {
+    return y;
+  } else (z > (x && y)) 
+    return z;
+  }
+console.log(maxOfThree(8, 25, 37));*/
+
+//#3
+/*function isCharAVowel () {
+  if (A || E || I || O || U) {
+    console.log(true);
+    } else {
+    return false;
+    }
+  }*/
+
+//#4
+/*function sumArray(array) {
+  let sum = 0;
+  array.forEach(item => {
+    sum += item;
+  });
+  console.log(sum);
+  return sum;
+}
+sumArray([5, 8, 65, 5, 12]);*/
+
+
+//#5
+/*function multiplyArray(array) {
+  let sum = 1;
+  array.forEach(item => {
+    sum *= item;
+  });
+  console.log(sum);
+  return sum;
+}
+multiplyArray([9, 2, 3]);*/
+
+//#6
+/*function numArgs(a, b, c, d) {
+  console.log(numArgs.length);
+}
+numArgs(5, 6, 7, 7);*/
